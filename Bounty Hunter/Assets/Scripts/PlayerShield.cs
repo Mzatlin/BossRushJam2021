cedd968cac6ee5b9712af77d2b611f62154ca931
Mzatlin <michael.zatlin@yahoo.com>
2021-07-02 11:08:02 -0700

    Feature: Added a delay for when you can use the shield again, and an uptime for how long you can hold the shield up. Added a simple animation for shielding.

--- Bounty Hunter/Assets/Scripts/PlayerShield.cs
+++ Bounty Hunter/Assets/Scripts/PlayerShield.cs
@@ -6,10 +6,19 @@ public class PlayerShield : MonoBehaviour
 {
     IPlayerStats stats;
     IHittablle hit;
+    Animator anim;
     bool isShieldActive = false;
+    [SerializeField] float protectionAmount = 2f;
+    [SerializeField] float timeBeforeShield = 0.4f;
+    float timeThreshold = 0f;
+    Coroutine shieldDelay;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponentInChildren<Animator>();
         hit = GetComponent<IHittablle>();
         stats = GetComponent<IPlayerStats>();
     }
@@ -22,18 +31,47 @@ public class PlayerShield : MonoBehaviour
             hit.CanHit = false;
         }
 
-        if (stats != null && hit != null && stats.GetPlayerReadiness() && !isShieldActive && Input.GetMouseButton(1))
+        if (stats != null && hit != null && stats.GetPlayerReadiness() && !isShieldActive && Input.GetMouseButtonDown(1))
         {
-            stats.SetPlayerReadiness(false);
-            isShieldActive = true;
-            hit.CanHit = false;
+            if(timeThreshold < Time.time)
+            {
+                shieldDelay = StartCoroutine(ShieldUpTime());
+            }
+
+
         }
         else if(isShieldActive && Input.GetMouseButtonUp(1))
         {
-            stats.SetPlayerReadiness(true);
-            isShieldActive = false;
-            hit.CanHit = true;
-        }
+            if (shieldDelay != null)
+            {
+                StopCoroutine(shieldDelay);
+            }
 
+            DisableShield();
+        }
+    }
+
+    IEnumerator ShieldUpTime()
+    {
+        EnableShield();
+        yield return new WaitForSeconds(protectionAmount);
+        DisableShield();
+    }
+
+    void EnableShield()
+    {
+        stats.SetPlayerReadiness(false);
+        isShieldActive = true;
+        hit.CanHit = false;
+        anim.SetTrigger("Shield");
+    }
+
+    void DisableShield()
+    {
+        stats.SetPlayerReadiness(true);
+        isShieldActive = false;
+        hit.CanHit = true;
+        anim.SetTrigger("Shield");
+        timeThreshold = Time.time + timeBeforeShield;
     }
 }
