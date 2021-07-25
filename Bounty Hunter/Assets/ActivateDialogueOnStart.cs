530a5943920aea184a2a3a104014766ee65a8c60
Mzatlin <michael.zatlin@yahoo.com>
2021-07-25 14:18:32 -0700

    Feature: Implemented a basic dialogue cutscene.

---
+++ Bounty Hunter/Assets/ActivateDialogueOnStart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ActivateDialogueOnStart : MonoBehaviour, IDialogueActivate
+{
+   
+    public event Action OnDialogueStart = delegate { };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1f);
+        OnDialogueStart();
+    }
+
+}
