8c5a2c615e3fc71e4445588cb84c58d720884c8e
Mzatlin <michael.zatlin@yahoo.com>
2021-07-27 22:05:09 -0700

    Refactor: Cleaned up Dash script and made it use the player's last direction instead of following the mouse. Added misc sprites.

--- Bounty Hunter/Assets/Scripts/Player/PlayerDash.cs
+++ Bounty Hunter/Assets/Scripts/Player/PlayerDash.cs
@@ -1,103 +1,112 @@
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerDash : MonoBehaviour
 {
-    Camera cam;
-    bool canDash = true;
-    bool isdashing = false;
-    float distance;
-    Vector2 mousePos;
-    Vector2 dashDirection;
-    Collider2D playerCollider;
-    float dashSpeed;
     IMovePhysics physics;
     IPlayerStats stats;
-    [SerializeField] float maxDashSpeed = 100f;
+    IMoveDirection direction;
+
+    Vector2 dashDirection = Vector2.zero;
+    Collider2D playerCollider;
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] float dashSpeed = 100f;
+    [SerializeField] float dashDuration = 0.5f;
     [SerializeField] LayerMask layer;
-    float timeThreshold = 0.4f;
-    float dashDelay;
+
+    float remainingDashTime = 0f;
+    float dashDelay = 0f;
+    bool isdashing = false;
+    float dashCooldown = 0.4f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main;
-        dashSpeed = maxDashSpeed;
         physics = GetComponent<IMovePhysics>();
         stats = GetComponent<IPlayerStats>();
+        direction = GetComponent<IMoveDirection>();
         playerCollider = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Time.time > dashDelay)
-        {
-            canDash = true;
-        }
-
-
         if (Input.GetKeyDown(KeyCode.Space) && stats != null && stats.GetPlayerReadiness())
         {
-            AttemptDash();
+            if (Time.time >= (dashDelay + dashCooldown))
+            {
+                GetDashDirection();
+                AttemptDash();
+            }
         }
         if (isdashing)
         {
             HandleDash();
         }
+        Ray2D ray = new Ray2D(transform.position, dashDirection);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
 
     }
 
     void HandleDash()
     {
-        distance = Vector2.Distance(mousePos, transform.position);
-        if (distance < 3f || dashSpeed < 5)
+        if (remainingDashTime > 0)
+        {
+            SetPlayerState(false);
+            remainingDashTime -= Time.deltaTime;
+            transform.position += (Vector3)(dashDirection * dashSpeed * Time.deltaTime);
+            // rb.MovePosition(rb.position + (dashDirection * dashSpeed * Time.deltaTime));
+            // rb.velocity = dashDirection * dashSpeed;
+            //rb.velocity = Vector2.left * dashSpeed;
+        }
+
+        if (remainingDashTime <= 0 || IsColliding())
         {
             isdashing = false;
-            dashSpeed = maxDashSpeed;
-            dashDelay = Time.time + timeThreshold;
-            if (stats != null)
-            {
-                stats.SetPlayerReadiness(true);
-            }
-            if (playerCollider != null)
-            {
-                playerCollider.enabled = true;
-            }
+            SetPlayerState(true);
         }
-        else
+
+    }
+
+    void AttemptDash()
+    {
+        if (!isdashing)
         {
-            transform.position += (Vector3)(dashDirection * dashSpeed * Time.deltaTime);
-            dashSpeed -= dashSpeed * 10f * Time.deltaTime;
+            isdashing = true;
+            dashDelay = Time.time;
+            remainingDashTime = dashDuration;
         }
+    }
+
+    void SetPlayerState(bool isActive)
+    {
+        if (stats != null)
+        {
+            stats.SetPlayerReadiness(isActive);
+        }
+        if (playerCollider != null)
+        {
+            playerCollider.enabled = isActive;
+        }
+    }
+
+    void GetDashDirection()
+    {
+        dashDirection = direction.LastMovementDirection;// - (Vector2)transform.position).normalized;//(mousePos - (Vector2)transform.position).normalized;
     }
 
     bool IsColliding()
     {
         Ray2D ray = new Ray2D(transform.position, dashDirection);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 5f, layer);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 2f, layer);
         if (hit)
         {
+            Debug.Log("Hit");
             return false;
         }
         return true;
-    }
-
-    void AttemptDash()
-    {
-        if (canDash && !isdashing)
-        {
-            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-            dashDirection = (mousePos - (Vector2)transform.position).normalized;
-            isdashing = true;
-            canDash = false;
-            if (stats != null)
-            {
-                stats.SetPlayerReadiness(false);
-            }
-            if (playerCollider != null)
-            {
-                playerCollider.enabled = false;
-            }
-        }
     }
 }
