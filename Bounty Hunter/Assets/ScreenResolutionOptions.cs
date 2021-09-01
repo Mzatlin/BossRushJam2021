e85503aa55923b2d6293f295f2ce265613553af1
Mzatlin <michael.zatlin@yahoo.com>
2021-08-31 23:58:55 -0700

    Feature: Added basic dropdown that lists all resolution types available.

---
+++ Bounty Hunter/Assets/ScreenResolutionOptions.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScreenResolutionOptions : MonoBehaviour
+{
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        Resolution currentRes;
+        int index = 0;
+        foreach(var res in resolutions)
+        {
+            options.Add(res.width + " x " + res.height);
+            if(res.width == Screen.currentResolution.width && res.height == Screen.currentResolution.height)
+            {
+                currentRes = res;
+            }
+            else
+            {
+                index++;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = index;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
