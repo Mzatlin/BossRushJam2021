4372408415273e1648212e1dca09eaa61e2997a1
Mzatlin <michael.zatlin@yahoo.com>
2021-10-26 22:19:51 -0700

    Fix: Resolution options no longer show duplicates (lower refresh rates) in the options menu. Fixed more grammar errors in opening cutscene dialogue.

--- Bounty Hunter/Assets/Scripts/UI/ScreenResolutionOptions.cs
+++ Bounty Hunter/Assets/Scripts/UI/ScreenResolutionOptions.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+
+using ScreenDimensions = System.Tuple<int, int>;
 
 public class ScreenResolutionOptions : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
     Resolution[] resolutions;
+    HashSet<ScreenDimensions> uniqResolutions = new HashSet<ScreenDimensions> ();
+    Dictionary<ScreenDimensions, int> maxRefreshRates = new Dictionary<ScreenDimensions, int>();
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +24,29 @@ public class ScreenResolutionOptions : MonoBehaviour
         int index = 0;
         foreach(var res in resolutions)
         {
+            ScreenDimensions resolution = new ScreenDimensions(res.width, res.height);
+            uniqResolutions.Add(resolution);
+            if (maxRefreshRates.ContainsKey(resolution))
+            {
+                maxRefreshRates[resolution] = res.refreshRate;
+            }
+            else
+            {
+                maxRefreshRates.Add(resolution, res.refreshRate);
+            }
+        }
+        foreach(ScreenDimensions resolution in uniqResolutions)
+        {
+            Resolution res = new Resolution();
+            res.width = resolution.Item1;
+            res.height = resolution.Item2;
+            if(maxRefreshRates.TryGetValue(resolution,out int refreshRate))
+            {
+                res.refreshRate = refreshRate;
+            }
+
             options.Add(res.width + " x " + res.height);
-            if(res.width == Screen.currentResolution.width && res.height == Screen.currentResolution.height)
+            if (res.width == Screen.currentResolution.width && res.height == Screen.currentResolution.height)
             {
                 currentRes = res;
             }
@@ -29,6 +55,7 @@ public class ScreenResolutionOptions : MonoBehaviour
                 index++;
             }
         }
+
 
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = index;
